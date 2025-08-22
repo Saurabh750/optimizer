17a50a2e7b853161c850d41e9e2d41b1157131e1
Saurabh750 <saurabhpage1@gmail.com>
2025-08-22 23:30:13 +0530

    minor change

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='muons',
-    version='0.1.4',
+    version='0.1.5',
     author='Saurabh Page',
     author_email='saurabhpage1@gmail.com',
     description='Muon opimizers',
